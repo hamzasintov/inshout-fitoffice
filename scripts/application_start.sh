585b6a5cbb453fc3600c31a20c5252bdd4f132b6
Muhammad Hamza <hamza@sintov.com>
2024-04-21 01:24:17 +0500

    pipeline test check

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,5 +3,10 @@
 # navigate to project folder
 cd /home/ubuntu/inshout-fitOffice
 
+# To stop already running project
+pm2 stop inshout-fitOffice
+
+pm2 delete inshout-fitOffice
+
 # for project start
-npm run start
+pm2 start --name inshout-fitOffice npm -- start
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,3 +8,4 @@ sudo apt-get install curl
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install nodejs -y
 sudo apt-get install npm -y
+sudo npm install -g pm2
